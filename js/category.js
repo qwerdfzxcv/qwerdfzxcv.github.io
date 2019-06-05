f4b8eb07cdb016ae87ae4f4edceb21014d134c2d
dengfenglaii <1297312606@qq.co>
2019-06-06 02:45:04 +0800

    update

---
+++ js/category.js
@@ -0,0 +1,8 @@
+/*区域滚动
+条件：一个容器装着一个容器的html结构，找到大容器,子容器大于父容器*/
+window.onload = function () {
+   new IScroll(document.querySelector(".cateMain_left"),{
+       scrollX:false,
+       scrollY:true
+   })
+};
